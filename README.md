21ea75d2f65d26b76dd5d04eb3f0e77fe470adae
Blake Stauffer <77029920+bestauffer@users.noreply.github.com>
2022-09-19 18:54:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Exercise 2-3: Create a simple program
+
+### INSTRUCTIONS
+
+Create a simple program on your own to ask for the length and width of a rectangle/square. 
+
+Then calculate the area and perimeter and display the results. 
+
+There is no starting file for this activity. Use best practices to create a title, proper inputs and output. 
+
+Consider user experience in your program design. 
+
+## Example output
+
+```
+The Area and Perimeter program
+
+Please enter the length: 25
+Please enter the width: 10
+
+Area = 250
+Perimeter = 70
+
+Thanks for using this program! 
---
+++ main.py
@@ -0,0 +1,29 @@
+# Exercise 2-3: Create a simple program
+
+# INSTRUCTIONS
+
+# Create a simple program on your own to ask for the length and width of a rectangle/square. 
+
+# Then calculate the area and perimeter and display the results. 
+
+# There is no starting file for this activity.
+
+# Consider user experience and output - include a title, and a farewell greeting. 
+print("Lets find the area & perimeter of a rectangle!!!")
+print()
+length = int(input('Enter the length: '))
+width = int(input('Enter the width:  '))
+area = length * width
+perimeter = 2*(length + width)
+print("====================Results====================")
+if length == width:
+  print("It's a square!!!")
+else:
+  print("It's a rectangle!!!")
+print()
+print("length:", length)
+print("width:", width)
+print("area:", area)
+print("perimeter:", perimeter)
+print()
+print("Goodbye")
